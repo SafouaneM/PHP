20b5aa9a910928880f2e88d649c0258be59c3158
SafouaneM <safouane_m@Hotmail.nl>
2018-04-10 15:26:45 +0200

    Opdracht 004

---
+++ Cijfers.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: safou
+ * Date: 10-4-2018
+ * Time: 14:51
+ */
+
+function telop($a, $b) {
+
+    $c = $a + $b;
+    return $a. " + " .$b. " = " .$c;
+}
+
+echo telop(a: 30, b: 32 z);
+
