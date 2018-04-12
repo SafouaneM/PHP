22d4cad9d65f999f19b9da59f0c88b649bd73d3d
SafouaneM <safouane_m@Hotmail.nl>
2018-04-12 14:57:33 +0200

    Opdracht 004

---
+++ response.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: safou
+ * Date: 11-4-2018
+ * Time: 12:46
+ */
+//print_r($_GET);
+
+
+print_r($_POST['voetballers']);
+
+foreach($_POST['voetballers'] as $bal) {
+    echo "img src='../Shuffle".$bal.".jpg'>";
+}
